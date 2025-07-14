7caa38c61fc04f068a7335f093242985d43c1256
João Varela <joaocvarela@gmail.com>
2025-07-14 17:49:24 +0100

    Removed QiimeASV to its own file and reverted changes to QiimeParser parse()
    QiimeASV now includes QiimeTaxonomy struct as well

---
+++ blast_parser/BlastOutputParser/QiimeASV.swift
@@ -0,0 +1,42 @@
+//
+//  QiimeASV.swift
+//  blast_parser
+//
+//  Created by João Varela on 14/07/2025.
+//
+
+struct QiimeTaxonomy: CustomStringConvertible {
+    let taxonomy: String
+    let confidence: String
+    
+    init(taxonomy: String, confidence: String) {
+        self.taxonomy = taxonomy
+        self.confidence = confidence.trimmingCharacters(in: .newlines)
+    }
+    
+    var description: String {
+        return "\(taxonomy)\t\(confidence)"
+    }
+}
+
+struct QiimeASV: CustomStringConvertible {
+	let featureID: String
+	let samples: [String]
+	var taxonomy = [QiimeTaxonomy]()
+    
+    init(featureID: String, samples: [String], taxonomy:QiimeTaxonomy) {
+		self.featureID = featureID
+		self.samples = samples
+        self.taxonomy.append(taxonomy)
+	}
+    
+    mutating func add(taxonomy:QiimeTaxonomy) {
+        self.taxonomy.append(taxonomy)
+    }
+    
+    var description: String {
+        let samplesDescription = samples.joined(separator: "\t")
+        let taxonomyDescription = taxonomy.map { $0.description }.joined(separator: "\t")
+        return "\(featureID)\t\(taxonomyDescription)\t\(samplesDescription)"
+    }
+}
--- blast_parser/BlastOutputParser/QiimeParser.swift
+++ blast_parser/BlastOutputParser/QiimeParser.swift
@@ -6,67 +6,27 @@
 //
 import Foundation
 
-//parses Qiime2 merged.tsv file
-
-struct QiimeASV: CustomStringConvertible {
-	let featureID: String
-	let samples: [String]
-	var taxonomy = [QiimeTaxonomy]()
-    
-    init(featureID: String, samples: [String], taxonomy:QiimeTaxonomy) {
-		self.featureID = featureID
-		self.samples = samples
-        self.taxonomy.append(taxonomy)
-	}
-    
-    mutating func add(taxonomy:QiimeTaxonomy) {
-        self.taxonomy.append(taxonomy)
-    }
-    
-    var description: String {
-        let samplesDescription = samples.joined(separator: "\t")
-        let taxonomyDescription = taxonomy.map { $0.description }.joined(separator: "\t")
-        return "\(featureID)\t\(taxonomyDescription)\t\(samplesDescription)"
-    }
-}
-
-struct QiimeTaxonomy: CustomStringConvertible {
-    let taxonomy: String
-    let confidence: String
-    
-    init(taxonomy: String, confidence: String) {
-        self.taxonomy = taxonomy
-        self.confidence = confidence.trimmingCharacters(in: .newlines)
-    }
-    
-    var description: String {
-        return "\(taxonomy)\t\(confidence)"
-    }
-}
-
-final class QiimeParser: FilesParser {
+final class QiimeParser: FileParser {
 	var lines = [QiimeASV]()
 	func parse() throws -> [QiimeASV] {
-        for parser in self.parsers {
-            var index = 0
-            for line in parser.readStream {
-                if index == 0 {
-                    // validation
-                    let cleanLine = line.trimmingCharacters(in: .newlines)
-                    let header = cleanLine.replacingOccurrences(of: "-", with: "CN").components(separatedBy: "\t")
-                    guard header.contains("id"), header.contains("Taxon"),
-                          header.contains("Confidence") else
-                        { throw RuntimeError("Invalid Qiime 2 merged file") }
-                    
-                    let asv = getASV(line: header, count: header.count)
-                    lines.append(asv)
-                } else if index > 1 {
-                    let items = line.components(separatedBy: "\t")
-                    let asv = getASV(line: items, count: items.count)
-                    lines.append(asv)
-                }
-                index += 1
+        var index = 0
+        for line in readStream {
+            if index == 0 {
+                // validation
+                let cleanLine = line.trimmingCharacters(in: .newlines)
+                let header = cleanLine.replacingOccurrences(of: "-", with: "CN").components(separatedBy: "\t")
+                guard header.contains("id"), header.contains("Taxon"),
+                      header.contains("Confidence") else
+                    { throw RuntimeError("Invalid Qiime 2 merged file") }
+                
+                let asv = getASV(line: header, count: header.count)
+                lines.append(asv)
+            } else if index > 1 {
+                let items = line.components(separatedBy: "\t")
+                let asv = getASV(line: items, count: items.count)
+                lines.append(asv)
             }
+            index += 1
         }
 		
 		return lines
