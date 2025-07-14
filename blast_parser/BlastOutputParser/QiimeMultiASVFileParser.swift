6d2d518bb109173ab8d56b8d8c5c21093123ee7f
João Varela <joaocvarela@gmail.com>
2025-07-14 23:18:37 +0100

    Ended the implementation of QiimeMultiASVFileParser

--- blast_parser/BlastOutputParser/QiimeMultiASVFileParser.swift
+++ blast_parser/BlastOutputParser/QiimeMultiASVFileParser.swift
@@ -9,8 +9,35 @@ import Foundation
 
 struct QiimeASVFile {
     let path: String
-    let asvs:[QiimeASV]
+    var asvs:[QiimeASV]
     let headerPrefix: String
+    
+    init(path: String, asvs: [QiimeASV]) {
+        self.path = path
+        self.asvs = asvs
+        let url = URL(fileURLWithPath: path, isDirectory: false)
+        let dirURL = url.deletingLastPathComponent()
+        // the default prefix will be the directory name
+        self.headerPrefix = dirURL.lastPathComponent
+    }
+    
+    mutating func parseHeader() throws {
+        let header = asvs.first?.description
+        guard let header = header
+            else { throw RuntimeError("Parsing of a Qiime file header failed.") }
+        let headers = header.components(separatedBy: "\t")
+        let taxonPrefixedHeaders = headers.map
+            { $0.replacingOccurrences(of: "Taxon",
+                                      with: "\(headerPrefix)-Taxon") }
+        let confidencePrefixedHeaders = taxonPrefixedHeaders.map
+            { $0.replacingOccurrences(of: "Confidence",
+                                      with: "\(headerPrefix)-Confidence") }
+       
+        let prefixedHeaders = confidencePrefixedHeaders.map { String($0) }
+        let prefixedHeader = QiimeASV(components: prefixedHeaders)
+        asvs.removeFirst()
+        asvs.insert(prefixedHeader, at: 0)
+    }
 }
 
 final class QiimeMultiASVFileParser {
@@ -22,7 +49,44 @@ final class QiimeMultiASVFileParser {
         guard parsers.count == pathsArray.count else { return nil }
     }
     
-    func parse() throws -> [QiimeASV] {
+    func merge() throws -> [QiimeASV] {
+        var files = [QiimeASVFile]()
         
+        // parse
+        for parser in parsers {
+            let asvs = try parser.parse()
+            var file = QiimeASVFile(path: parser.path, asvs: asvs)
+            try file.parseHeader()
+            files.append(file)
+        }
+        
+        guard files.isEmpty == false else {
+            throw RuntimeError("Merging of Qiime files failed: no files found.")
+        }
+        
+        // check if the files can be merged
+        var count = files[0].asvs.count
+        for file in files[1...] {
+            guard count == file.asvs.count else {
+                throw RuntimeError("Merging of Qiime files failed: " +
+                                   "files have different number of ASVs.")
+            }
+        }
+        
+        // merge
+        var mergedASVs = files[0].asvs
+        
+        for file in files[1...] {
+            for (i, asv) in file.asvs.enumerated() {
+                guard mergedASVs[i].featureID == asv.featureID else {
+                    throw RuntimeError("Merging of Qiime files failed: " +
+                                       "ASV IDs do not match.")
+                }
+                
+                mergedASVs[i].add(taxonomy: asv.taxonomy[0])
+            }
+        }
+        
+        return mergedASVs
     }
 }
