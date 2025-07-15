4711048fe5fe0a9b9dc8a0f860b83c970525a499
João Varela <joaocvarela@gmail.com>
2025-07-15 17:41:02 +0100

    Improved the help file of the merge-qiime subcommand

--- blast_parser/BlastParser.swift
+++ blast_parser/BlastParser.swift
@@ -207,7 +207,7 @@ extension BlastParser {
 	}
 	struct MergeQiime: ParsableCommand {
 		static let configuration = CommandConfiguration(
-			abstract: "Merges a Qiime2 merge.tsv file with the best hits of a BLAST search.",
+			abstract: "Merges one or more tab-separated Qiime2 ASV read count files with the best hits of a BLAST search.",
 			usage: "blast_parser merge-qiime --asvs <asvs> --blasthits <blasthits> [--output <output>] [--hits-per-asv <hits-per-asv>] [--sort <sort>]",
 			aliases: ["mrgq"] )
 		
