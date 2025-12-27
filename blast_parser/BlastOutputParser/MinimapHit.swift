9058214c8b5836883b1ad44fd365e7399639116a
João Varela <joaocvarela@gmail.com>
2025-12-27 17:38:14 +0000

    Started to build the minimap2 output parser

---
+++ blast_parser/BlastOutputParser/MinimapHit.swift
@@ -0,0 +1,35 @@
+//
+//  MinimapHit.swift
+//  blast_parser
+//
+//  Created by João Varela on 27/12/2025.
+//
+
+import Foundation
+
+struct MinimapHit: CustomStringConvertible {
+    let queryID: String
+    let referenceID: String
+    let alignmentScore: Int
+    let alignmentLength: Int
+    let taxonomy: String
+    
+    var description: String {
+        "\(queryID)\t\(referenceID)\t\(alignmentScore)\t\(alignmentLength)\t\(taxonomy)"
+    }
+    
+    init(from line: String) throws {
+        let components = line.split(separator: "\t").map(String.init)
+        guard components.count == 5,
+              let alignmentScore = Int(components[2]),
+              let alignmentLength = Int(components[3]) else {
+            throw RuntimeError("Invalid Minimap hit: \(line)")
+        }
+        
+        self.queryID = components[0]
+        self.referenceID = components[1]
+        self.alignmentScore = alignmentScore
+        self.alignmentLength = alignmentLength
+        self.taxonomy = components[4]
+    }
+}
---
+++ blast_parser/BlastOutputParser/MinimapParser.swift
@@ -0,0 +1,34 @@
+//
+//  MinimapParser.swift
+//  blast_parser
+//
+//  Created by João Varela on 27/12/2025.
+//
+
+import Foundation
+
+final class MinimapParser: FileParser {
+    func parse() throws -> [MinimapHit] {
+        var hits = [MinimapHit]()
+        var index = 0
+        
+        for line in readStream {
+            if index == 0 {
+                // validation
+                let header = line.split(separator: "\t")
+                guard header.count == 5 else {
+                    throw RuntimeError("Invalid header line for minimap2 classification file")
+                }
+                guard header.contains("Query_ID"), header.contains("Reference_ID"),  header.contains("Alignment_Score"), header.contains("Alignment_length"), header.contains("Taxonomy") else {
+                    throw RuntimeError("Invalid header: It should contain the following columns: Query_ID, Reference_ID, Alignment_Score, Alignment_length, Taxonomy")
+                }
+                index += 1
+            } else if index > 0 {
+                let hit = try MinimapHit(from: line)
+                hits.append(hit)
+            }
+        }
+        
+        return hits
+    }
+}
