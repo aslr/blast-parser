9e05d912ac67aa9e6e555c1b986b6bf2bdfd22e2
João Varela <joaocvarela@gmail.com>
2025-12-30 18:29:49 +0000

    Started to implement the MergeMinimap subcommand.

---
+++ blast_parser/BlastOutputParser/BlastMinimapOutputParser.swift
@@ -0,0 +1,13 @@
+//
+//  BlastMinimapOutputParser.swift
+//  blast_parser
+//
+//  Created by João Varela on 30/12/2025.
+//
+
+import Foundation
+
+final class BlastMinimapOutputParser: BlastOutputParser {
+    
+    
+}
